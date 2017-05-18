64f761b68084a581dcd280ce1d6b688206821236
Sebastian Werner <s.werner@sebastian-software.de>
2017-05-18 23:27:45 +0200

    Tweaked browserlist + improved tests

--- packages/edge-postcss/__tests__/lost.js
+++ packages/edge-postcss/__tests__/lost.js
@@ -1,12 +1,22 @@
 import postcss from "postcss"
 import loadConfig from "postcss-load-config"
 
-test("Render Lost Grid", () => {
-  let css = "h1 { lost-column: 3/12 }"
+test("Lost Grid", () => {
+  let input = ".grid { lost-column: 3/12 }"
 
   return loadConfig({ map: "inline" }).then(({ plugins, options }) => {
     return postcss(plugins)
-      .process(css, options)
+      .process(input, options)
+      .then((result) => expect(result.css).toMatchSnapshot())
+  })
+})
+
+test("Autoprefixer", () => {
+  let input = ":fullscreen a { display: flex }"
+
+  return loadConfig({ map: "inline" }).then(({ plugins, options }) => {
+    return postcss(plugins)
+      .process(input, options)
       .then((result) => expect(result.css).toMatchSnapshot())
   })
 })
