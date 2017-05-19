42e25f5cba13c81589bed9022cc17dc2df7e54a5
Sebastian Werner <s.werner@sebastian-software.de>
2017-05-19 13:53:28 +0200

    Refactored test runner

--- packages/edge-postcss/__tests__/main.js
+++ packages/edge-postcss/__tests__/main.js
@@ -1,40 +1,49 @@
 import postcss from "postcss"
 import loadConfig from "postcss-load-config"
 
-function css(input) {
-  return loadConfig({ map: false }).then(({ plugins, options }) => {
-    return postcss(plugins)
-      .process(input, options)
-      .then((result) => expect(result.css).toMatchSnapshot())
+let plugins = null
+let options = null
+
+beforeAll(() =>
+  loadConfig({ map: false }).then((config) => {
+    plugins = config.plugins
+    options = config.options
+    return true
   })
+)
+
+function compile(input) {
+  return postcss(plugins)
+    .process(input, options)
+    .then((result) => expect(result.css).toMatchSnapshot())
 }
 
 test("Lost Grid", () =>
-  css(".grid { lost-column: 3/12 }")
+  compile(".grid { lost-column: 3/12 }")
 )
 
 test("Autoprefixer", () =>
-  css(":fullscreen a { display: flex }")
+  compile(":fullscreen a { display: flex }")
 )
 
 test("Mixins", () =>
-  css("@mixin simple{ color: red; } h1 { @include simple; }")
+  compile("@mixin simple{ color: red; } h1 { @include simple; }")
 )
 
 test("Variables", () =>
-  css("$bgColor: red; h1 { background: $bgColor; }")
+  compile("$bgColor: red; h1 { background: $bgColor; }")
 )
 
 test("Nested", () =>
-  css("body { h1 { font-weight: bold; } h2 { font-weight: normal; }}")
+  compile("body { h1 { font-weight: bold; } h2 { font-weight: normal; }}")
 )
 
 test("Nested Parent Selector", () =>
-  css("body { ul { li { &:first-child { margin-top: 0; }}}}")
+  compile("body { ul { li { &:first-child { margin-top: 0; }}}}")
 )
 
 test("Grid KISS", () =>
-  css(`
+  compile(`
     body {
     	grid-kiss:
     		"+-------------------------------+      "
@@ -54,17 +63,17 @@ test("Grid KISS", () =>
 )
 
 test("Calc Trivial", () =>
-  css(".elem { width: calc(300px + 10px); }")
+  compile(".elem { width: calc(300px + 10px); }")
 )
 
 test("Calc Variable", () =>
-  css("$margin: 10px; .elem { width: calc(200px + $margin); }")
+  compile("$margin: 10px; .elem { width: calc(200px + $margin); }")
 )
 
 test("Calc Keep", () =>
-  css(".elem { width: calc(100px + 2%); }")
+  compile(".elem { width: calc(100px + 2%); }")
 )
 
 test("zIndex", () =>
-  css(".first { z-index: 1000; } .second { z-index: 2000; }")
+  compile(".first { z-index: 1000; } .second { z-index: 2000; }")
 )
