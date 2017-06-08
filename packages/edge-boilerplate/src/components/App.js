cb3953e987b397ad0b1c45bfc4e764f2f24c5555
James Gillmore <james@faceyspacey.com>
2017-06-08 04:27:10 -0700

    feat($first):

---
+++ packages/edge-boilerplate/src/components/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import universal from 'react-universal-component'
+import styles from '../css/App.css'
+
+const UniversalExample = universal(() => import('./Example'), {
+  resolve: () => require.resolveWeak('./Example'),
+  minDelay: 500
+})
+
+export default class App extends React.Component {
+  // set `show` to `true` to see dynamic chunks served by initial request
+  // set `show` to `false` to test how asynchronously loaded chunks behave,
+  // specifically how css injection is embedded in chunks + corresponding HMR
+  state = {
+    show: true
+  }
+
+  componentDidMount() {
+    if (this.state.show) return
+
+    setTimeout(() => {
+      console.log('now showing <Example />')
+      this.setState({ show: true })
+    }, 1500)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className={styles.title}>Hello World</h1>
+        {this.state.show && <UniversalExample />}
+        {!this.state.show && 'Async Component Not Requested Yet'}
+      </div>
+    )
+  }
+}
