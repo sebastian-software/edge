f4726ed33af54f11b2ea6f9978e9f2676cd3d74b
Sebastian Werner <s.werner@sebastian-software.de>
2017-06-26 09:33:11 +0200

    Extracted ports into config

--- packages/edge-boilerplate/config.js
+++ packages/edge-boilerplate/config.js
@@ -10,3 +10,6 @@ export const PUBLIC_PATH = "/static/"
 
 export const SERVER_ENTRY = resolve(ROOT, "src/server/index.js")
 export const CLIENT_ENTRY = resolve(ROOT, "src/client/index.js")
+
+export const DEVELOPMENT_PORT = 1339
+export const PRODUCTION_PORT = 1339
--- packages/edge-boilerplate/server/index.js
+++ packages/edge-boilerplate/server/index.js
@@ -3,33 +3,28 @@ import webpack from "webpack"
 import webpackDevMiddleware from "webpack-dev-middleware"
 import webpackHotMiddleware from "webpack-hot-middleware"
 import webpackHotServerMiddleware from "webpack-hot-server-middleware"
-
+import { CLIENT_OUTPUT, PUBLIC_PATH, DEVELOPMENT_PORT, PRODUCTION_PORT } from "../config"
 import configBuilder from "../webpack/builder"
-const clientConfig = configBuilder({
-  target: "client",
-  env: process.env.NODE_ENV
-})
-const serverConfig = configBuilder({
-  target: "server",
-  env: process.env.NODE_ENV
-})
-
-const publicPath = clientConfig.output.publicPath
-const outputPath = clientConfig.output.path
-
-
 
 const server = express()
 
-const PORT = 3000
-
 if (process.env.NODE_ENV === "development") {
+
+  const clientConfig = configBuilder({
+    target: "client",
+    env: process.env.NODE_ENV
+  })
+  const serverConfig = configBuilder({
+    target: "server",
+    env: process.env.NODE_ENV
+  })
+
   const multiCompiler = webpack([ clientConfig, serverConfig ])
   const clientCompiler = multiCompiler.compilers[0]
 
   server.use(webpackDevMiddleware(multiCompiler, {
     // required
-    publicPath,
+    publicPath: PUBLIC_PATH,
 
     // display no info to console (only warnings and errors)
     noInfo: true
@@ -39,16 +34,20 @@ if (process.env.NODE_ENV === "development") {
 
   // keeps serverRender updated with arg: { clientStats, outputPath }
   server.use(webpackHotServerMiddleware(multiCompiler, {
-    serverRendererOptions: { outputPath }
+    serverRendererOptions: {
+      outputPath: CLIENT_OUTPUT
+    }
   }))
+
+  server.listen(DEVELOPMENT_PORT, () => {})
 }
 else
 {
   const clientStats = require("../build/client/stats.json")
   const serverRender = require("../build/server/main.js").default
 
-  server.use(publicPath, express.static(outputPath))
-  server.use(serverRender({ clientStats, outputPath }))
-}
+  server.use(PUBLIC_PATH, express.static(CLIENT_OUTPUT))
+  server.use(serverRender({ clientStats, CLIENT_OUTPUT }))
 
-server.listen(PORT, () => {})
+  server.listen(PRODUCTION_PORT, () => {})
+}
