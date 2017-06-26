799c66952c3764746b0e6d7cbac0161380218947
Sebastian Werner <s.werner@sebastian-software.de>
2017-06-26 12:12:32 +0200

    Tweaked code

--- packages/edge-boilerplate/server/react.js
+++ packages/edge-boilerplate/server/react.js
@@ -3,11 +3,12 @@ import { CLIENT_OUTPUT, SERVER_OUTPUT, PUBLIC_PATH, PRODUCTION_PORT } from "../c
 
 /* eslint-disable no-console */
 /* eslint-disable import/no-commonjs */
+/* eslint-disable security/detect-non-literal-require */
 
 const server = express()
 
-const clientStats = require(CLIENT_OUTPUT + "/stats.json")
-const serverRender = require(SERVER_OUTPUT + "/main.js").default
+const clientStats = require(`${CLIENT_OUTPUT}/stats.json`)
+const serverRender = require(`${SERVER_OUTPUT}/main.js`).default
 
 server.use(PUBLIC_PATH, express.static(CLIENT_OUTPUT))
 server.use(serverRender({ clientStats, CLIENT_OUTPUT }))
