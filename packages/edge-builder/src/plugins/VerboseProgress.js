0defb1127465948ba9cfd1431de5fa32b8c69143
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-11 23:16:16 +0200

    Made some simplifications and feedback adjustments. Overall the whole percent idea is so totally fake that it's not worth to keep it.

--- packages/edge-builder/src/plugins/VerboseProgress.js
+++ packages/edge-builder/src/plugins/VerboseProgress.js
@@ -6,131 +6,111 @@
 /* eslint-disable no-magic-numbers */
 
 import ora from "ora"
+import { get as getRoot } from "app-root-dir"
+import { relative } from "path"
+
+const ROOT = getRoot()
 
 export default class VerboseProgress {
   apply(compiler) {
-    let lastModulesCount = 0
-    let moduleCount = 500
     let doneModules = 0
     let spinner = null
-    var prevPercent = 0
-    const activeModules = []
+    let lastModule = null
 
-    function handler(percentage, message) {
-      if (percentage < 1) {
-        spinner.text = `${Math.floor(percentage * 100)}% ${message}`
+    function display(message) {
+      if (message !== "") {
+        spinner.text = message
 
         // We somehow have to force rendering otherwise busy Webpack wouldn't let us.
-        if (percentage !== prevPercent) {
-          spinner.render()
-          prevPercent = percentage
-        }
+        spinner.render()
       } else {
         spinner.succeed("Done!")
       }
     }
 
-    function update(module) {
-      handler(
-        0.1 + doneModules / Math.max(lastModulesCount, moduleCount) * 0.6,
-        `building modules ${doneModules}/${moduleCount}`
-      )
-    }
-
     function moduleDone(module) {
-      doneModules++
-      const ident = module.identifier()
-      if (ident) {
-        const idx = activeModules.indexOf(ident)
-        if (idx >= 0) activeModules.splice(idx, 1)
-      }
-      update()
+      doneModules += 1
+
+      let humanIndex = lastModule.lastIndexOf("!")
+      let humanModuleId = humanIndex === -1 ? lastModule : lastModule.slice(humanIndex + 1, lastModule.length)
+
+      humanModuleId = relative(ROOT, humanModuleId).replace(/^node_modules\//, "~/")
+
+      display(`Building Modules ${humanModuleId} - Done: ${doneModules}...`)
     }
 
     compiler.plugin("compilation", (compilation) => {
-      if (compilation.compiler.isChild()) return
-      lastModulesCount = moduleCount
-      moduleCount = 0
+      if (compilation.compiler.isChild()) {
+        return
+      }
+
       doneModules = 0
 
       spinner = ora({ interval: 16 })
       spinner.start()
 
-      handler(0, "compiling")
+      display(0, "compiling")
 
       compilation.plugin("build-module", (module) => {
-        moduleCount++
-        const ident = module.identifier()
-        if (ident) {
-          activeModules.push(ident)
-        }
-        update()
+        lastModule = module.identifier()
       })
 
       compilation.plugin("failed-module", moduleDone)
       compilation.plugin("succeed-module", moduleDone)
 
       const syncHooks = {
-        "seal": [ 0.71, "sealing" ],
-        "optimize": [ 0.72, "optimizing" ],
-        "optimize-modules-basic": [ 0.73, "basic module optimization" ],
-        "optimize-modules": [ 0.74, "module optimization" ],
-        "optimize-modules-advanced": [ 0.75, "advanced module optimization" ],
-        "optimize-chunks-basic": [ 0.76, "basic chunk optimization" ],
-        "optimize-chunks": [ 0.77, "chunk optimization" ],
-        "optimize-chunks-advanced": [ 0.78, "advanced chunk optimization" ],
-        "optimize-chunk-modules": [ 0.8, "chunk modules optimization" ],
-        "optimize-chunk-modules-advanced": [ 0.81, "advanced chunk modules optimization" ],
-        "revive-modules": [ 0.82, "module reviving" ],
-        "optimize-module-order": [ 0.83, "module order optimization" ],
-        "optimize-module-ids": [ 0.84, "module id optimization" ],
-        "revive-chunks": [ 0.85, "chunk reviving" ],
-        "optimize-chunk-order": [ 0.86, "chunk order optimization" ],
-        "optimize-chunk-ids": [ 0.87, "chunk id optimization" ],
-        "before-hash": [ 0.88, "hashing" ],
-        "before-module-assets": [ 0.89, "module assets processing" ],
-        "before-chunk-assets": [ 0.9, "chunk assets processing" ],
-        "additional-chunk-assets": [ 0.91, "additional chunk assets processing" ],
-        "record": [ 0.92, "recording" ]
+        "seal": "Sealing",
+        "optimize": "Optimizing",
+        "optimize-modules-basic": "Optimizing modules",
+        "optimize-chunks-basic": "Optimizing chunks",
+        "optimize-chunk-modules": "Optimizing chunk modules",
+        "optimize-module-order": "Optimizing module order",
+        "optimize-module-ids": "Optimizing module ids",
+        "optimize-chunk-order": "Optimizing chunk order",
+        "optimize-chunk-ids": "Optimizing chunk ids",
+        "before-hash": "Hashing",
+        "before-module-assets": "Processing module assets",
+        "before-chunk-assets": "Processing chunk assets",
+        "record": "Recording"
       }
 
       Object.keys(syncHooks).forEach((name) => {
         let pass = 0
-        const settings = syncHooks[name]
+        const message = syncHooks[name]
         compilation.plugin(name, () => {
-          if (pass++ > 0) handler(settings[0], settings[1], `pass ${pass}`)
-          else handler(settings[0], settings[1])
+          if (pass++ > 0) display(message + ` [pass ${pass}]`)
+          else display(message)
         })
       })
 
       compilation.plugin("optimize-tree", (chunks, modules, callback) => {
-        handler(0.79, "module and chunk tree optimization")
+        display("Optimizing tree")
         callback()
       })
 
       compilation.plugin("additional-assets", (callback) => {
-        handler(0.91, "additional asset processing")
+        display("Processing assets")
         callback()
       })
 
       compilation.plugin("optimize-chunk-assets", (chunks, callback) => {
-        handler(0.92, "chunk asset optimization")
+        display("Optimizing chunk assets")
         callback()
       })
 
       compilation.plugin("optimize-assets", (assets, callback) => {
-        handler(0.94, "asset optimization")
+        display("Optimizing assets")
         callback()
       })
     })
 
     compiler.plugin("emit", (compilation, callback) => {
-      handler(0.95, "emitting")
+      display("Emitting")
       callback()
     })
 
     compiler.plugin("done", () => {
-      handler(1, "")
+      display("")
     })
   }
 }
