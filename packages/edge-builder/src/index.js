959e0f8a78cd80c85947156127b60f9983f16413
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-14 15:45:22 +0200

    Added notification API

--- packages/edge-builder/src/index.js
+++ packages/edge-builder/src/index.js
@@ -2,8 +2,14 @@ import gulp from "gulp"
 import runSequence from "run-sequence"
 import { addDevMiddleware } from "./express/dev"
 import dotenv from "dotenv"
+import notifier from "node-notifier"
+import chalk from "chalk"
+
 import { cleanClient, cleanServer, buildClient, buildServer } from "./commands/build"
 import { startDevServer } from "./commands/dev"
+
+// Initialize environment configuration
+dotenv.config()
 
 export {
   addDevMiddleware,
@@ -11,8 +17,34 @@ export {
   startDevServer
 }
 
-// Initialize environment configuration
-dotenv.config()
+export function createNotification(options)
+{
+  const title = `${options.title}`
+
+  if (options.notify) {
+    notifier.notify({
+      title,
+      message: options.message
+    })
+  }
+
+  const level = options.level || "info"
+  const message = `${chalk.bold(title)}: ${options.message}`
+
+  switch (level) {
+    case "warn":
+      console.log(chalk.yellow(message))
+      break
+
+    case "error":
+      console.log(chalk.bgRed.white(message))
+      break
+
+    case "info":
+    default:
+      console.log(message)
+  }
+}
 
 export function addGulpTasks() {
   gulp.task("clean:server", cleanServer)
