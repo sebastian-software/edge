57169122d9f80a1426fe21d62afb2278d747f16e
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-14 18:36:12 +0200

    Moved express related code to edge-builder

--- packages/edge-core/src/server.js
+++ packages/edge-core/src/server.js
@@ -2,8 +2,4 @@
 
 export * from "./common"
 
-export { default as createExpressServer } from "./server/createExpressServer"
-export { default as createUniversalMiddleware } from "./server/createUniversalMiddleware"
-export { default as addFallbackHandler } from "./server/addFallbackHandler"
 export { default as renderPage } from "./server/renderPage"
-export { enableEnhancedStackTraces, logError } from "./server/debug"
