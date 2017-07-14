c83ac1615d98de8c4bd62635124733eafa45f85f
Sebastian Werner <s.werner@sebastian-software.de>
2017-07-14 18:42:47 +0200

    Improved jsdoc

--- packages/edge-core/src/server/renderPage.js
+++ packages/edge-core/src/server/renderPage.js
@@ -2,18 +2,16 @@ import serialize from "serialize-javascript"
 import Helmet from "react-helmet"
 
 /**
- * Generates a full HTML page containing the render output of the given react
- * element.
+ * Generates a full HTML page containing the render output of the given react element.
  *
- * @param config {Object}
- * - rootReactElement: [Optional] The root React element to be rendered on the page.
- * - initialState: [Optional] The initial state for the redux store which will be used by the
+ * @param config {Object} Configuration.
+ * @param config.html {string} The rendered HTML content.
+ * @param config.initialState {Object} [{}] The initial state for the redux store which will be used by the
  *   client to mount the redux store into the desired state.
- * - nonce {string}:
- * - locale {string}:
- * - styles {string}:
- * - scripts {string}:
- *
+ * @param config.nonce {string} [""] Nonce for securing origin.
+ * @param config.locale {string} [""] Selected locale to send to the user.
+ * @param config.styles {string} [""] Styles to inject into the page.
+ * @param config.scripts {string} [""] Scripts to inject into the page.
  * @returns The full HTML page in the form of a React element.
  */
 export default function renderPage({ html, initialState = {},
