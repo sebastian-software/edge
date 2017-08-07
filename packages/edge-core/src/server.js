65fd3aa12ecbc6560019cde51386173da2006a2a
Sebastian Werner <s.werner@sebastian-software.de>
2017-08-07 16:05:13 +0200

    Moved debug code block from edge-builder

--- packages/edge-core/src/server.js
+++ packages/edge-core/src/server.js
@@ -4,3 +4,4 @@ export * from "./common"
 
 export { default as renderPage } from "./server/renderPage"
 export { default as getLocaleData } from "./server/getLocaleData"
+export * from "./server/debug"
