6ffdae56b8bcaeea2ed640c7dd9ea1ce277e3b0d
Sebastian Werner <s.werner@sebastian-software.de>
2017-08-15 18:01:58 +0200

    Added optional load param to 'loadImport' + added API docs.

--- packages/edge-core/src/client/loadImport.js
+++ packages/edge-core/src/client/loadImport.js
@@ -1,3 +1,11 @@
-export default function loadImport(wrapped) {
-  return wrapped.then((module) => module.default)
+/**
+ * Acts on transpiled `import()` statements from
+ * [babel-plugin-universal-import](https://www.npmjs.com/package/babel-plugin-universal-import).
+ *
+ * @param {Object} wrapped The return value from transpiled `import()` statements.
+ * @param {boolean} load Whether to actual load the imported file, otherwise just tracks that it's being imported.
+ * @returns {Promise} Promise which resolves with the default import of imported file (asynchronous, lazy loaded).
+ */
+export default function loadImport(wrapped, load = true) {
+  return load ? wrapped.then((module) => module.default) : Promise.resolve(null)
 }
--- packages/edge-core/src/server/loadImport.js
+++ packages/edge-core/src/server/loadImport.js
@@ -1,7 +1,15 @@
 /* global __webpack_require__ */
 import { CHUNK_NAMES } from "react-universal-component"
 
-export default function loadImport(wrapped) {
+/**
+ * Acts on transpiled `import()` statements from
+ * [babel-plugin-universal-import](https://www.npmjs.com/package/babel-plugin-universal-import).
+ *
+ * @param {Object} wrapped The return value from transpiled `import()` statements.
+ * @param {boolean} load Whether to actual load the imported file, otherwise just tracks that it's being imported.
+ * @returns {any} The default export of the imported file (synchronously loaded).
+ */
+export default function loadImport(wrapped, load = true) {
   CHUNK_NAMES.add(wrapped.chunkName())
-  return __webpack_require__(wrapped.resolve()).default
+  return load ? __webpack_require__(wrapped.resolve()).default : null
 }
