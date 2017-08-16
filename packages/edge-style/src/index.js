0b204007ab7d2d3261058c9ea3486a885523814b
Sebastian Werner <s.werner@sebastian-software.de>
2017-08-16 23:05:00 +0200

    Worked on new structure for publishing

--- packages/edge-style/src/index.js
+++ packages/edge-style/src/index.js
@@ -1,1 +1,7 @@
-import "./index.css"
+import "./Normalize.css"
+import "./Reset.css"
+import "./Sanitize.css"
+import "./BoxSizing.css"
+import "./Aria.css"
+import "./OpenType.css"
+import "./Intl.css"
