847d94aab82ffaf1bcd01b0000fc55c27649c521
Sebastian Werner <s.werner@sebastian-software.de>
2017-08-21 12:51:23 +0200

    Formatted

--- packages/edge-useragent/bin/testfiles.js
+++ packages/edge-useragent/bin/testfiles.js
@@ -1,24 +1,27 @@
 #!/usr/bin/env node
 
-import request from 'request';
-import path from 'path';
-import fs from 'fs';
+import request from "request"
+import path from "path"
+import fs from "fs"
 
 const files = {
-    'pgts.yaml': 'https://raw.githubusercontent.com/ua-parser/uap-core/master/test_resources/pgts_browser_list.yaml'
-  , 'testcases.yaml': 'https://raw.githubusercontent.com/ua-parser/uap-core/master/test_resources/test_user_agent_parser.yaml'
-  , 'firefoxes.yaml': 'https://raw.githubusercontent.com/ua-parser/uap-core/master/test_resources/firefox_user_agent_strings.yaml'
-};
+  "pgts.yaml":
+    "https://raw.githubusercontent.com/ua-parser/uap-core/master/test_resources/pgts_browser_list.yaml",
+  "testcases.yaml":
+    "https://raw.githubusercontent.com/ua-parser/uap-core/master/test_resources/test_user_agent_parser.yaml",
+  "firefoxes.yaml":
+    "https://raw.githubusercontent.com/ua-parser/uap-core/master/test_resources/firefox_user_agent_strings.yaml"
+}
 
 /**
  * Update the fixtures
  */
 
 Object.keys(files).forEach(key => {
-  request(files[key], function response (err, res, data) {
-    if (err || res.statusCode !== 200) return console.error('failed to update');
+  request(files[key], function response(err, res, data) {
+    if (err || res.statusCode !== 200) return console.error("failed to update")
 
-    console.log('downloaded', files[key]);
-    fs.writeFileSync(path.join(__dirname, '..', 'tests', 'fixtures', key), data);
-  });
-});
+    console.log("downloaded", files[key])
+    fs.writeFileSync(path.join(__dirname, "..", "tests", "fixtures", key), data)
+  })
+})
