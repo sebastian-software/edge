0efb7cfaf8085aba2f782a51d515c4a082d005aa
Sebastian Werner <s.werner@sebastian-software.de>
2017-08-21 12:51:35 +0200

    Merged features into main bundle

--- packages/edge-useragent/src/index.js
+++ packages/edge-useragent/src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+import semver from 'semver';
 
 /**
  * This is where all the magic comes from, specially crafted for `useragent`.
@@ -146,6 +146,18 @@ class Agent {
 
     if (version) output += ` ${version}`;
     return output;
+  }
+
+  /**
+   * Checks if the user agent's version can be satisfied agents the give
+   * ranged argument. This uses the semver libraries range construction.
+   *
+   * @param {String} ranged The range the version has to satisfie
+   * @returns {Boolean}
+   * @api public
+   */
+  satisfies(range) {
+    return semver.satisfies(`${Number(this.major) || 0}.${Number(this.minor) || 0}.${Number(this.patch) || 0}`, range);
   }
 
   /**
