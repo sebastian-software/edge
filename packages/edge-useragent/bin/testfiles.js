0c7eba2c77e6c7814a52dd1bea651a1ab6f0eaff
Sebastian Werner <s.werner@sebastian-software.de>
2017-08-21 13:57:18 +0200

    Corrected downloader + removed non existing path

--- packages/edge-useragent/bin/testfiles.js
+++ packages/edge-useragent/bin/testfiles.js
@@ -7,8 +7,6 @@ import fs from "fs"
 const files = {
   "pgts.yaml":
     "https://raw.githubusercontent.com/ua-parser/uap-core/master/test_resources/pgts_browser_list.yaml",
-  "testcases.yaml":
-    "https://raw.githubusercontent.com/ua-parser/uap-core/master/test_resources/test_user_agent_parser.yaml",
   "firefoxes.yaml":
     "https://raw.githubusercontent.com/ua-parser/uap-core/master/test_resources/firefox_user_agent_strings.yaml"
 }
@@ -22,6 +20,6 @@ Object.keys(files).forEach(key => {
     if (err || res.statusCode !== 200) return console.error("failed to update")
 
     console.log("downloaded", files[key])
-    fs.writeFileSync(path.join(__dirname, "..", "tests", "fixtures", key), data)
+    fs.writeFileSync(path.join(__dirname, "..", "test", "fixtures", key), data)
   })
 })
