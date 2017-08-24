570c0e5600bddce1dd605a4f00fd3973cf0b0504
Sebastian Werner <s.werner@sebastian-software.de>
2017-08-24 16:49:01 +0200

    Reworked for usage of kernel class.

--- packages/edge-core/src/common/wrapApplication.js
+++ packages/edge-core/src/common/wrapApplication.js
@@ -1,30 +1,45 @@
 import React from "react"
+
 import { Provider } from "react-redux"
 import { ApolloProvider } from "react-apollo"
 import { IntlProvider } from "react-intl"
 
-export default function wrapApplication(Application, { locale, apolloClient, reduxStore }) {
+/**
+ * Wraps the application class with different providers for offering the
+ * following features:
+ *
+ * - Apollo GraphQL
+ * - Redux
+ * - React Intl
+ *
+ * This might be extended with new features during development.
+ *
+ * @param {React.Component} Application The React root application component.
+ * @param {Kernel} kernel Kernel instance which holds the data oriented runtime state.
+ * @returns {React.Component} Returns the wrapped application component.
+ */
+export default function wrapApplication(Application, kernel) {
   let Wrapped = Application
 
-  if (apolloClient) {
+  if (kernel.apolloClient) {
     Wrapped = (
-      <ApolloProvider client={apolloClient} store={reduxStore}>
+      <ApolloProvider client={kernel.apolloClient} store={kernel.reduxStore}>
         {Wrapped}
       </ApolloProvider>
     )
   }
 
-  if (reduxStore) {
+  if (kernel.reduxStore) {
     Wrapped = (
-      <Provider store={reduxStore}>
+      <Provider store={kernel.reduxStore}>
         {Wrapped}
       </Provider>
     )
   }
 
-  if (locale) {
+  if (kernel.intl) {
     Wrapped = (
-      <IntlProvider locale={locale}>
+      <IntlProvider locale={kernel.intl.locale}>
         {Wrapped}
       </IntlProvider>
     )
