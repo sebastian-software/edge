30ffa255fa919c7274ced7815d07871725546fbc
Sebastian Werner <s.werner@sebastian-software.de>
2017-08-28 16:31:27 +0200

    Minor changes

--- packages/edge-core/src/client/renderApp.js
+++ packages/edge-core/src/client/renderApp.js
@@ -1,7 +1,9 @@
+import React from "react"
 import { render } from "react-dom"
+
 import wrapApplication from "../common/wrapApplication"
 
 export default function renderApp(Application, kernel) {
   console.log("[EDGE]: Rendering application...")
-  render(wrapApplication(Application, kernel), document.getElementById("root"))
+  render(wrapApplication(<Application />, kernel), document.getElementById("root"))
 }
--- packages/edge-core/src/common/fetchData.js
+++ packages/edge-core/src/common/fetchData.js
@@ -5,12 +5,12 @@ export default async function fetchData(WrappedApplication, kernel) {
   // Supports parallel loading of either Apollo-style (aka fetchData())
   // and Redux-First-Router aka thunks attached to paths
   const start = new Date()
-  console.log("[EDGE]: Fetching data...")
+  console.log("[EDGE] Fetching data...")
   const result = await Promise.all([
     kernel.router.thunk(kernel.store),
     deepFetch(WrappedApplication)
   ])
 
-  console.log(`[EDGE]: Fetching done in ${new Date() - start}ms`)
+  console.log(`[EDGE] Done in ${new Date() - start}ms`)
   return result
 }
