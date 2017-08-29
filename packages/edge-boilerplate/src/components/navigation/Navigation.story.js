b50080854bd0014bf87e1f64b27bce7e0ad3d449
Sebastian Werner <s.werner@sebastian-software.de>
2017-08-29 21:56:12 +0200

    Added first test

---
+++ packages/edge-boilerplate/src/components/navigation/Navigation.story.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { storiesOf } from "@storybook/react"
+import Navigation from "./Navigation"
+
+storiesOf("Navigation", module)
+  .add("basic demo", () => <Navigation />)
