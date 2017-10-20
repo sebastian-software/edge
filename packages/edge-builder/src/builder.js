b2a92a5c71d7d5dbf78991588134be82d9a9d5ed
Sebastian Werner <s.werner@sebastian-software.de>
2017-10-20 18:07:48 +0200

    Adapted to changed signature of uglify plugin.

--- packages/edge-builder/src/builder.js
+++ packages/edge-builder/src/builder.js
@@ -388,9 +388,7 @@ export default function builder(target, env = "development", config = {}) {
       config.build.bundleCompression === "uglify" && isProduction && isClient ?
         new UglifyPlugin({
           sourceMap: config.build.enableSourceMaps,
-          parallel: {
-            cache: true
-          },
+          parallel: true,
           uglifyOptions: UGLIFY_OPTIONS
         }) : null,
 
