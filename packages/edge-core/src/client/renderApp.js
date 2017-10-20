c04d1fe41c0b92c00d1fe0da2b46a94a66a1aa12
Sebastian Werner <s.werner@sebastian-software.de>
2017-10-20 12:36:09 +0200

    Adjusted API usage for deprecation of 'render'

--- packages/edge-core/src/client/renderApp.js
+++ packages/edge-core/src/client/renderApp.js
@@ -1,9 +1,9 @@
 import React from "react"
-import { render } from "react-dom"
+import { hydrate } from "react-dom"
 
 import wrapApplication from "../common/wrapApplication"
 
 export default function renderApp(Application, kernel) {
   console.log("[EDGE]: Rendering application...")
-  render(wrapApplication(<Application />, kernel), document.getElementById("root"))
+  hydrate(wrapApplication(<Application />, kernel), document.getElementById("root"))
 }
