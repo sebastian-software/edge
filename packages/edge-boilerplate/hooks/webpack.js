e69653366da54023548f2be0715fccb9f5725ce3
Sebastian Werner <s.werner@sebastian-software.de>
2017-11-03 14:37:57 +0100

    Added example for Webpack hook

---
+++ packages/edge-boilerplate/hooks/webpack.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/unambiguous, import/no-commonjs */
+module.exports = function webpack(config, { isServer, isClient, isProduction, isDevelopment }) {
+  // Extend and manipulate the Webpack configuration here.
+  return config
+}
