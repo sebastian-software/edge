1afb6a54c187506ba4faf44fb9eac853f7280f60
Sebastian Werner <s.werner@sebastian-software.de>
2017-11-06 21:07:30 +0100

    Moved dev methods for webpack into webpack folder.

--- packages/edge-builder/src/index.js
+++ packages/edge-builder/src/index.js
@@ -1,13 +1,15 @@
 import { loadConfig } from "./config"
 import { cleanClient, cleanServer, buildClient, buildServer } from "./commands/build"
-import { connectWithWebpack, createMiddleware } from "./commands/dev"
+import { connectWithWebpack, createMiddleware } from "./webpack/dev"
 
 export {
   loadConfig,
+
   cleanClient,
   cleanServer,
   buildClient,
   buildServer,
+
   connectWithWebpack,
   createMiddleware
 }
