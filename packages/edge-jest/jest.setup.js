03586306c81f98c5d657b9b8867bac944ced415a
Sebastian Werner <s.werner@sebastian-software.de>
2018-05-23 15:25:34 +0200

    Added jest-mock-now for mocking Dates inside Jest.

--- packages/edge-jest/jest.setup.js
+++ packages/edge-jest/jest.setup.js
@@ -3,5 +3,8 @@
 // Mocking all fetch() calls. Should never depend on any actual network during test.
 global.fetch = require("jest-fetch-mock")
 
+// Mock Date.now() so that all values are static
+require("jest-mock-now")("2018-05-17T11:25:51.054Z")
+
 // Polyfill for RequestAnimationFrame which is required for React v16
 require("raf/polyfill")
