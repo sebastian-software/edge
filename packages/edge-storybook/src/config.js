8bc701e8818e98868b1875cbf73b92324d3731ae
Sebastian Werner <s.werner@sebastian-software.de>
2018-05-24 18:06:49 +0200

    Added support for dynamically load react-intl data in Storybook.

--- packages/edge-storybook/src/config.js
+++ packages/edge-storybook/src/config.js
@@ -5,7 +5,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux"
 import { Provider } from "react-redux"
 import { configure, addDecorator } from "@storybook/react"
 import { connectRoutes } from "redux-first-router"
-import { IntlProvider } from "react-intl"
+import { IntlProvider, addLocaleData } from "react-intl"
 import Cookie from "js-cookie"
 
 import "edge-style"
@@ -52,26 +52,35 @@ const store = createStore(reducers, {}, enhancers)
 // via some UI to switch between different locales.
 const locale = Cookie.get("locale") || "en-US"
 
-addDecorator((story) => {
-  return (
-    <IntlProvider locale={locale}>
-      <Provider store={store}>{story()}</Provider>
-    </IntlProvider>
-  )
+const language = locale.split("-")[0]
+
+// Dynamically loading configured language support
+import(`react-intl/locale-data/${language}`).then((data) => {
+  // Access CJS data by using "default" key
+  addLocaleData(data.default)
+  console.log("React-Intl loaded data for", language)
+
+  addDecorator((story) => {
+    return (
+      <IntlProvider locale={locale}>
+        <Provider store={store}>{story()}</Provider>
+      </IntlProvider>
+    )
+  })
+
+  // Uses the injected ROOT from our Webpack config to find stories
+  // relative to the application folder.
+
+  // 1. Require all initializers files e.g. core CSS required for all components, i18n setup, etc.
+  const initLoader = require.context(global.APP_SRC, false, /\bInit\.js$/)
+  const initializers = initLoader.keys().map(initLoader)
+
+  console.log("Loaded", initializers.length, "initializers.")
+
+  // 2. Find and load all stories found somewhere in the application directory.
+  const storyLoader = require.context(global.APP_SRC, true, /\.story\.js$/)
+  const stories = storyLoader.keys().map(storyLoader)
+  configure(() => stories, module)
+
+  console.log("Added", stories.length, "stories.")
 })
-
-// Uses the injected ROOT from our Webpack config to find stories
-// relative to the application folder.
-
-// 1. Require all initializers files e.g. core CSS required for all components, i18n setup, etc.
-const initLoader = require.context(global.APP_SRC, false, /\bInit\.js$/)
-const initializers = initLoader.keys().map(initLoader)
-
-console.log("Loaded", initializers.length, "initializers.")
-
-// 2. Find and load all stories found somewhere in the application directory.
-const storyLoader = require.context(global.APP_SRC, true, /\.story\.js$/)
-const stories = storyLoader.keys().map(storyLoader)
-configure(() => stories, module)
-
-console.log("Added", stories.length, "stories.")
