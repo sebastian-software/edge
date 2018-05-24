dd3fa9b01b185b3fb04733b0f1abcf60e0fa2991
Sebastian Werner <s.werner@sebastian-software.de>
2018-05-24 20:49:49 +0200

    Fixed init of jest-mock-now

--- packages/edge-jest/jest.setup.js
+++ packages/edge-jest/jest.setup.js
@@ -4,7 +4,7 @@
 global.fetch = require("jest-fetch-mock")
 
 // Mock Date.now() so that all values are static
-require("jest-mock-now")("2018-05-17T11:25:51.054Z")
+require("jest-mock-now")(new Date("2018-05-17T11:25:51.054Z"))
 
 // Polyfill for RequestAnimationFrame which is required for React v16
 require("raf/polyfill")
