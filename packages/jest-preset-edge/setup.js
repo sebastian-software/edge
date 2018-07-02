d1f5969327a042294c42dc8a5e8b0c67ccc1625a
Sebastian Werner <s.werner@sebastian-software.de>
2018-07-02 16:14:30 +0200

    Mocking some console methods when running inside jest

--- packages/jest-preset-edge/setup.js
+++ packages/jest-preset-edge/setup.js
@@ -1,7 +1,15 @@
 /* eslint-disable import/no-commonjs, filenames/match-regex */
+/* global jest */
 
 // Mocking all fetch() calls. Should never depend on any actual network during test.
 global.fetch = require("jest-fetch-mock")
+
+// Rewrite console.log/debug to a mock. This shouldn't be required to run in tests
+// and makes test runner output much more calm and focused. We keep warn/error and
+// other not so often used methods intact.
+console.clear = jest.fn()
+console.log = jest.fn()
+console.debug = jest.fn()
 
 // Mock Date.now() so that all values are static
 require("jest-mock-now")(new Date("2018-05-17T11:25:51.054Z"))
