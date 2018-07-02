a45be97d34d4795334d7a881b9a46b18054468b1
Sebastian Werner <s.werner@sebastian-software.de>
2018-07-02 16:01:01 +0200

    Added addons for jest and story sources.

--- packages/edge-storybook/src/addons.js
+++ packages/edge-storybook/src/addons.js
@@ -3,3 +3,5 @@
 import "@storybook/addon-actions/register"
 import "@storybook/addon-links/register"
 import "@storybook/addon-viewport/register"
+import "@storybook/addon-jest/register"
+import "@storybook/addon-storysource/register"
