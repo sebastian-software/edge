fc1119000bb2c12b7635b88315fd7302c40c5ac7
Sebastian Werner <s.werner@sebastian-software.de>
2018-07-10 18:03:01 +0200

    Some auto formatting of storybook config + made message loading optional (to not crash when directory does not exist.)

--- packages/edge-storybook/src/config.js
+++ packages/edge-storybook/src/config.js
@@ -1,14 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
-
-import React from "react"
-import { configure, addDecorator } from "@storybook/react"
-import { IntlProvider, addLocaleData } from "react-intl"
 import Cookie from "js-cookie"
 
 // The "global" import fixes issues accessing globals from outside of the VM
 // where this script is running. This is mainly relevant for running Storyshots via Jest.
 /* eslint-disable no-global-assign, no-native-reassign */
 import global from "global"
+import React from "react"
+import { addDecorator, configure } from "@storybook/react"
+import { addLocaleData, IntlProvider } from "react-intl"
 
 // Core Reset Styles
 import "edge-style"
@@ -35,14 +34,21 @@ if (process.env.NODE_ENV === "test" && require.context == null) {
   /* global __dirname */
   require("babel-plugin-require-context-hook/register")()
   require.context = function context(directory, useSubdirectories, regExp) {
-    return global.__requireContext(__dirname, directory, useSubdirectories, regExp)
+    return global.__requireContext(
+      __dirname,
+      directory,
+      useSubdirectories,
+      regExp
+    )
   }
 }
 
 // Using same cookie as in our applications which should make it possible
 // via some UI to switch between different locales.
-let locale = process.env.NODE_ENV === "test" ?
-  process.env.APP_DEFAULT_LOCALE : Cookie.get("locale")
+let locale =
+  process.env.NODE_ENV === "test" ?
+    process.env.APP_DEFAULT_LOCALE :
+    Cookie.get("locale")
 
 if (locale == null) {
   locale = "en-US"
@@ -57,25 +63,46 @@ console.log("React-Intl loaded data for", language)
 
 // Loading messages for components from application root "messages" folder
 // Expect that these messages are named `${language}.json` or `${locale}.json`.
-const messageLoader = require.context(process.env.APP_SOURCE + "/messages", !1, /\.json$/)
+let messageLoader
+try {
+  messageLoader = require.context(
+    `${process.env.APP_SOURCE}/messages`,
+    !1,
+    /\.json$/
+  )
+} catch(error) {}
 
-const localeMatcher = new RegExp(locale + "\\.json$")
-const languageMatcher = new RegExp(language + "\\.json$")
+let messages = {}
 
-const localeSpecific = messageLoader.keys().filter((messageFile) => localeMatcher.test(messageFile))
-const languageSpecific = messageLoader.keys().filter((messageFile) => languageMatcher.test(messageFile))
+if (messageLoader) {
+  const localeMatcher = new RegExp(`${locale}\\.json$`)
+  const languageMatcher = new RegExp(`${language}\\.json$`)
 
-const localeData = localeSpecific[0] ? messageLoader(localeSpecific[0]) : {}
-const languageData = languageSpecific[0] ? messageLoader(languageSpecific[0]) : {}
+  const localeSpecific = messageLoader
+    .keys()
+    .filter((messageFile) => localeMatcher.test(messageFile))
+  const languageSpecific = messageLoader
+    .keys()
+    .filter((messageFile) => languageMatcher.test(messageFile))
 
-// We merge the data from locale and language specific files - if both are avaible.
-// Locale specific messages override language only data.
-const messages = { ...languageData, ...localeData }
+  const localeData = localeSpecific[0] ? messageLoader(localeSpecific[0]) : {}
+  const languageData = languageSpecific[0] ?
+    messageLoader(languageSpecific[0]) :
+    {}
+
+  // We merge the data from locale and language specific files - if both are avaible.
+  // Locale specific messages override language only data.
+  messages = { ...languageData, ...localeData }
+}
 
 // Decorate all stories with localization support so that FormattedMessage, etc. work correctly.
 addDecorator((story) => {
   return (
-    <IntlProvider locale={locale} messages={messages} defaultLocale={process.env.APP_DEFAULT_LOCALE}>
+    <IntlProvider
+      locale={locale}
+      messages={messages}
+      defaultLocale={process.env.APP_DEFAULT_LOCALE}
+    >
       {story()}
     </IntlProvider>
   )
@@ -91,7 +118,11 @@ const initializers = initLoader.keys().map(initLoader)
 console.log("Loaded", initializers.length, "initializers.")
 
 // 2. Find and load all stories found somewhere in the application directory.
-const storyLoader = require.context(process.env.APP_SOURCE, true, /\.story\.js$/)
+const storyLoader = require.context(
+  process.env.APP_SOURCE,
+  true,
+  /\.story\.js$/
+)
 const stories = storyLoader.keys().map(storyLoader)
 configure(() => stories, module)
 
